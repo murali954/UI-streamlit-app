0fa7fce3192472fdd85c4fc3ebad9df138200f4d
murali954 <151956416+murali954@users.noreply.github.com>
2024-10-26 02:12:53 -0700

    Update ui.py

--- ui.py
+++ ui.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from pydantic import BaseModel, root_validator
+from pydantic import BaseModel, model_validator
 
 from statsmodels.tsa.arima.model import ARIMA
 from sklearn.preprocessing import MinMaxScaler
